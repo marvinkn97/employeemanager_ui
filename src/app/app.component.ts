02239944a55deeaf9d178a4414190d1e214c14d4
Marvin Nyingi <nyingi.marvin@outlook.com>
2023-08-07 21:02:43 +0300

    added ngbootstrap and ui modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,47 @@
-import { HomeComponent } from './components/home/home.component';
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Employee } from './app.domain';
 import { EmployeeService } from './employee.service';
 import { NavComponent } from './components/nav/nav.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavComponent, HomeComponent],
+  imports: [CommonModule, NavComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   employees: Employee[] = [];
-  employeeService = inject(EmployeeService);
+  private employeeService = inject(EmployeeService);
+  private modalService = inject(NgbModal);
+  private fb = inject(FormBuilder);
+
+  constructor(){
+    this.fb.group({
+      name: [''],
+      email: [''],
+      jobTitle: [''],
+      phone: [''],
+      image: ['']
+    })
+  }
 
   ngOnInit(): void {
     this.getEmployees();
   }
 
   getEmployees(): void {
-    this.employeeService.getEmployees().subscribe(
-      (response: Employee[]) => {
-        this.employees = response;
-      }
-    );
+    this.employeeService.getEmployees().subscribe((response: Employee[]) => {
+      this.employees = response;
+    });
+  }
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then((result) => {});
   }
 }
